6bd242ae87994093d5c67b75c00d4053bd35c9f9
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-07-16 12:26:44 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,17 @@
+a=int(input())
+if a>=90:
+   print("grade A")
+elif a>=80:
+   print("grade B")
+elif a>=70:
+   print("grade C")
+elif a>=60:
+   print("grade D")
+elif a>=50:
+   print("grade E")
+else:
+   print("faild")      
+      
+     
+   
+      
