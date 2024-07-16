c2075149bd6960172ddb1451358806cc6ea6a781
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-07-16 12:28:08 +0530

    Create week.py

---
+++ week.py
@@ -0,0 +1,19 @@
+a=int(input())
+if (a==1):
+   print("MONDAY")
+elif (a==2):
+   print("TUESDAY")
+elif (a==3):
+   print("WEDNESDAY")
+elif (a==4):
+   print("THUSDAY")
+elif (a==5):
+   print("FRIDAY")
+elif (a==6):
+   print("SATURSDAY")
+elif (a==7):
+   print("SUNDAY")
+else:
+   print("invaild day")                  
+    
+  
