40eb0890234c2a465580edd31dd31c42304c1995
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-07-16 12:20:20 +0530

    Create largest of 3num.py

---
+++ lar of 3num.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c:
+    print("enter a is big")
+elif b>c and b>a:
+    print("enter b is big")
+else:
+    print("enter c is big")        
